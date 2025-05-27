735b71cb23cdd2a701c45de7d5680b6abf0fe853
Sattar Stamkul <sattar.coder@gmail.com>
2025-05-27 23:30:15 +0000

    updated: completed homework for module 2

--- datatalks_mlops_zoomcamp/module_2_mlflow/homework/train.py
+++ datatalks_mlops_zoomcamp/module_2_mlflow/homework/train.py
@@ -3,12 +3,13 @@ import pickle
 import click
 
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import root_mean_squared_error
 
 import mlflow
 
 
-MLFLOW_TRACKING_URI = "http://ec2-3-127-219-167.eu-central-1.compute.amazonaws.com:5000"
+# MLFLOW_TRACKING_URI = "http://ec2-3-127-219-167.eu-central-1.compute.amazonaws.com:5000"
+MLFLOW_TRACKING_URI = "sqlite:///mlflow.db"
 
 
 def load_pickle(filename: str):
@@ -39,7 +40,7 @@ def run_train(data_path: str):
         rf.fit(X_train, y_train)
         y_pred = rf.predict(X_val)
 
-        rmse = mean_squared_error(y_val, y_pred, squared=False)
+        rmse = root_mean_squared_error(y_val, y_pred)
 
 
 if __name__ == '__main__':
