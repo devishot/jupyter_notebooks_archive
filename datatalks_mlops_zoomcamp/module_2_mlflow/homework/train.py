0509070136442608b8a60559c6ab18fa0f85c002
Sattar Stamkul <sattar.coder@gmail.com>
2025-05-27 22:40:11 +0000

    added: homework into module 2 for MLOps course

---
+++ datatalks_mlops_zoomcamp/module_2_mlflow/homework/train.py
@@ -0,0 +1,46 @@
+import os
+import pickle
+import click
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+
+import mlflow
+
+
+MLFLOW_TRACKING_URI = "http://ec2-3-127-219-167.eu-central-1.compute.amazonaws.com:5000"
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+@click.command()
+@click.option(
+    "--data_path",
+    default="./output",
+    help="Location where the processed NYC taxi trip data was saved"
+)
+def run_train(data_path: str):
+
+    # Load the processed data
+
+    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+    mlflow.set_experiment("mlops-module-2-homework")
+
+    mlflow.sklearn.autolog()
+
+    with mlflow.start_run():
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+    run_train()
